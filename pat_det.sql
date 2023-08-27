dfcde7cc6cedd84499ec88159f4236f80e564386
siddh shah <102161086+Siddh0810@users.noreply.github.com>
2023-08-27 10:21:39 +0530

    Add files via upload

---
+++ pat_det.sql
@@ -0,0 +1,11 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.7.41-log 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `patient_details` (
+	`Ð°Þ` int (10),
+	`±Þ` varchar (60)
+); 
